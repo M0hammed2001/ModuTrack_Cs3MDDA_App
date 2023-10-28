ee325a7f0f231080d0328216f15d451474204a5a
Mohammed Sarhan <200150484@aston.ac.uk>
2023-10-28 19:48:51 +0100

    added train fragment map xml

--- app/src/main/java/uk/ac/aston/cs3mdd/mobiledesignproject/ui/Trainmap/TrainMapFragment.java
+++ app/src/main/java/uk/ac/aston/cs3mdd/mobiledesignproject/ui/Trainmap/TrainMapFragment.java
@@ -7,27 +7,46 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import uk.ac.aston.cs3mdd.mobiledesignproject.R;
 import uk.ac.aston.cs3mdd.mobiledesignproject.databinding.FragmentMapBinding;
 import uk.ac.aston.cs3mdd.mobiledesignproject.databinding.FragmentTrainmapBinding;
+import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.TrainService;
 
 public class TrainMapFragment extends Fragment {
 
     private FragmentTrainmapBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        TrainMapViewModel trainMapViewModel =
-                new ViewModelProvider(this).get(TrainMapViewModel.class);
+    private TrainService trainService;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TrainMapViewModel trainMapViewModel = new ViewModelProvider(this).get(TrainMapViewModel.class);
+
+        trainService = TrainMapFragmentArgs.fromBundle(getArguments()).getTrainervices();
 
         binding = FragmentTrainmapBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+//        final TextView textView = binding.textTrainmap;
+//        trainMapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return binding.getRoot();
+    }
 
-        final TextView textView = binding.textTrainmap;
-        trainMapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonTrainmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                NavHostFragment.findNavController(TrainMapFragment.this)
+                        .navigate(R.id.action_nav_trainmap_to_nav_train);
+            }
+        });
+
     }
 
     @Override
