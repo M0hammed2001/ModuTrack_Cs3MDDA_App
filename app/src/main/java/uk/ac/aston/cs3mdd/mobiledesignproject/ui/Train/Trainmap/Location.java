3c8a7f797dfe5a28e3223d7e8fb370d29bab4e2b
Mohammed Sarhan <200150484@aston.ac.uk>
2023-10-29 13:14:35 +0000

    Half way with creating the map, still getting erros

---
+++ app/src/main/java/uk/ac/aston/cs3mdd/mobiledesignproject/ui/Train/Trainmap/Location.java
@@ -0,0 +1,25 @@
+package uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.Trainmap;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.Serializable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.Destination;
+import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.TrainList;
+import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.TrainService;
+import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.service.TrainRepository;
+
+public class Location  extends Fragment {
+
+}
--- app/src/main/java/uk/ac/aston/cs3mdd/mobiledesignproject/ui/Train/Trainmap/TrainMapFragment.java
+++ app/src/main/java/uk/ac/aston/cs3mdd/mobiledesignproject/ui/Train/Trainmap/TrainMapFragment.java
@@ -1,9 +1,13 @@
 package uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.Trainmap;
 
+
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,15 +15,86 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLng;
+
 import uk.ac.aston.cs3mdd.mobiledesignproject.R;
 import uk.ac.aston.cs3mdd.mobiledesignproject.databinding.FragmentTrainmapBinding;
+import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.Destination;
 import uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI.TrainService;
 
-public class TrainMapFragment extends Fragment {
+public class TrainMapFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentTrainmapBinding binding;
-
     private TrainService trainService;
+    private  Location location;
+    private GoogleMap mMap;
+    public Destination destination;
+
+
+    private LatLng geocodeLocation(String locationName) {
+        // Implement geocoding logic here to get coordinates from the location name
+        // Example:
+        if (locationName.equals("Wolverhampton")) {
+            return new LatLng(52.5862, -2.1288);
+        } else if (locationName.equals("Telford Central")) {
+            return new LatLng(52.6815, -2.4463);
+        } else if (locationName.equals("Sandwell & Dudley")) {
+            return new LatLng(52.5023, -2.0214);
+        } else if (locationName.equals("Birmingham International")) {
+            return new LatLng(52.4520, -1.7381);
+        } else if (locationName.equals("Birmingham New Street")) {
+            return new LatLng(52.4520, -1.7381);
+        } else if (locationName.equals("Shrewsbury")) {
+            return new LatLng(52.7101, -2.7521);
+        } else if (locationName.equals("Walsall")) {
+            return new LatLng(52.5844, -1.9787);
+        } else if (locationName.equals("Bescot Stadium")) {
+            return new LatLng(52.5640, -1.9913);
+        } else if (locationName.equals("Tame Bridge Parkway")) {
+            return new LatLng(52.5466, -1.9741);
+        } else if (locationName.equals("Hamstead")) {
+            return new LatLng(52.5542, -1.9288);
+        } else if (locationName.equals("Perry Barr")) {
+            return new LatLng(52.5145, -1.8978);
+        } else if (locationName.equals("Witton")) {
+            return new LatLng(52.5093, -1.8849);
+        } else if (locationName.equals("Aston")) {
+            return new LatLng(52.5097, -1.8844);
+        } else {
+            // Shows a toast message advising mentioned location is not on the system yet.
+            Context context = requireContext(); // or use getActivity() if applicable
+            CharSequence text = "Location not found in the system";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            return new LatLng(0, 0); // Default to (0, 0) if not found
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Geocode the destination location name to get its coordinates (latitude and longitude)
+        LatLng loc = geocodeLocation(destination.getLocationName());
+
+        // Add a marker at the destination location and move the camera
+        mMap.addMarker(new MarkerOptions()
+                .position(loc)
+                .title("Location of this Station is"));
+
+        // Move the camera to the destination location
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 10));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         TrainMapViewModel trainMapViewModel = new ViewModelProvider(this).get(TrainMapViewModel.class);
@@ -27,28 +102,26 @@ public class TrainMapFragment extends Fragment {
         trainService = TrainMapFragmentArgs.fromBundle(getArguments()).getTrainervices();
 
         binding = FragmentTrainmapBinding.inflate(inflater, container, false);
-//        final TextView textView = binding.textTrainmap;
-//        trainMapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return binding.getRoot();
-
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         binding.buttonTrainmap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                NavHostFragment.findNavController(TrainMapFragment.this)
-                        .navigate(R.id.action_nav_trainmap_to_nav_train);
-            }
+         @Override
+          public void onClick(View view) {
+           NavHostFragment.findNavController(TrainMapFragment.this)
+                   .navigate(R.id.action_nav_trainmap_to_nav_train);
+           }
         });
-        binding.textTrainmap.setText(trainService.toString());
-
-
+    binding.textTrainmap.setText(trainService.toString());
     }
 
     @Override
